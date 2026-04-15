d66c0ae4ddb6ec26bf6f9eb8655b3c542e9349b0
hasti.movaliya <hasti.movaliya@450222-4>
2026-04-15 12:00:17 +0530

    jhgf

--- DeviceClient/obj/Debug/net8.0/DeviceClient.AssemblyInfo.cs
+++ DeviceClient/obj/Debug/net8.0/DeviceClient.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DeviceClient")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+41914eaddd0db302042331f65a132226b1ad9704")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba5245c0b06887ec63d67348e5d7e1e3423d13b3")]
 [assembly: System.Reflection.AssemblyProductAttribute("DeviceClient")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DeviceClient")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
