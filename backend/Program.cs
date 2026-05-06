96faab8e0d3e49779f36549b0a0a2387fe4e9cc6
Hatim <hatim.chharchhoda@matrixcomsec.com>
2026-05-06 17:55:49 +0530

    getting deploy error so taken to postgresql inbuilt given by render

--- backend/Program.cs
+++ backend/Program.cs
@@ -22,7 +22,8 @@ namespace COSEC_demo
             builder.Services.AddControllers();
 
             builder.Services.AddDbContext<AppDbContext>(options =>
-                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+                // options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+                options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
             builder.Services.AddScoped<JwtHelper>();
 
