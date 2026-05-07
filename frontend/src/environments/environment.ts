3489fea7c9778e0bba9687df2319d18b367a7283
Hatim <hatim.chharchhoda@matrixcomsec.com>
2026-05-07 09:30:36 +0530

    backend deploy frontend is not for that commit is done

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  apiUrl: 'http://localhost:5210/api'
+  //apiUrl: 'http://localhost:5210/api',
+  production: true,
+  apiUrl: 'https://cosec-demo-combine.onrender.com'
 };
